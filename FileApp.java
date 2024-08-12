740ec3380b0bdc99e3e4420cba816967466f936d
ktripathi2611 <tripathikushal522@gmail.com>
2024-08-12 23:19:33 +0530

    FileApp

--- FileApp.java
+++ FileApp.java
@@ -1,15 +1,15 @@
 import java.awt.*;
 
-class File {
-    File() {
+class FileApp {
+    FileApp() {
         Frame f = new Frame("File Scan and View Help");
-        MenuBar mb = new MenuBar(); 
+        MenuBar mb = new MenuBar();
 
         Menu fi = new Menu("File");
         Menu s = new Menu("Scan");
         Menu v = new Menu("View");
         Menu h = new Menu("Help");
-       
+
         MenuItem o = new MenuItem("Open");
         MenuItem n = new MenuItem("New");
         MenuItem sa = new MenuItem("Save");
@@ -17,11 +17,10 @@ class File {
         fi.add(o);
         fi.add(n);
         fi.add(sa);
-
-        mb.add(fi); 
-        mb.add(s);  
-        mb.add(v);  
-        mb.add(h);  
+        mb.add(fi);
+        mb.add(s);
+        mb.add(v);
+        mb.add(h);
 
         f.setMenuBar(mb);
         f.setSize(400, 400);
@@ -30,6 +29,6 @@ class File {
     }
 
     public static void main(String args[]) {
-        new File();
+        new FileApp();
     }
 }
